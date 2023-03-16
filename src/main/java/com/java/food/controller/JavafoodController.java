65e22150624600dc75db2585dc7a76d9fec553cf
이경용 <93perfect@naver.com>
2023-03-16 09:42:20 +0900

    오타

--- src/main/java/com/java/food/controller/JavafoodController.java
+++ src/main/java/com/java/food/controller/JavafoodController.java
@@ -14,12 +14,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.java.food.dto.FamousChartDTO;
 import com.java.food.dto.GenreDTO;
 import com.java.food.dto.PlayListDTO;
-import com.java.food.dto.login_DTO;
 import com.java.food.service.JavafoodService;
 
 @Controller
