ef51ab3a79d37b6fceb1f00b251d4224da671b50
이용준 <dldydwns0710@naver.com>
2023-03-21 16:13:12 +0900

    음악추가 페이지!!!

--- src/main/java/com/java/food/service/JavafoodServiceImpl.java
+++ src/main/java/com/java/food/service/JavafoodServiceImpl.java
@@ -531,33 +531,33 @@ public class JavafoodServiceImpl implements JavafoodService {
 		
 		@Override
 		public int insertsong(GenreDTO dto) {
-			int i=0;
-			try {
-				dto.getArtistname();
-				dto.getSongname();
-				dto.getLink();
-				dto.getAlbum_name();
-				dto.getBygenre();
-				dto.getPlaytime();
-				dto.getImagelink();
-				dto.getAlbum_add();
-				dto.getArtist_add();
-				dto.getCountry();			
-				i++;
-			} catch (Exception e) {
-			}
-			if(i==1) {
-				System.out.println("정상작동");
-				return javaDAO.insertsong(dto);
-			}
-			else {
-				System.out.println("0값");
-				return i;
-			}
 			
+			if(
+					dto.getArtistname() != null && dto.getArtistname() != "" && 
+					dto.getSongname() != null && dto.getSongname() != "" && 
+					dto.getLink() != null && dto.getLink() != "" &&
+					dto.getAlbum_name() != null && dto.getAlbum_name() != "" &&
+					dto.getBygenre() != null && dto.getBygenre() != "" &&
+					dto.getPlaytime() != null && dto.getPlaytime() != "" &&
+					dto.getImagelink() != null && dto.getImagelink() != "" &&
+					dto.getAlbum_add() != null && dto.getAlbum_add() != "" &&
+					dto.getArtist_add() != null && dto.getArtist_add() != "" &&
+					dto.getCountry() != null && dto.getCountry() != ""
+					
+				) {
+				System.out.println("server insert 입력 성공");
+				return javaDAO.insertsong(dto);
+				
+			} else {
+				System.out.println("server insert 모든 값을 입력하시오. ");
+			}
+			return 0;
 		}
-	
+			
+			
+}
+
 
 ////////////////////////////////////////////////////////////
 
-}
+
