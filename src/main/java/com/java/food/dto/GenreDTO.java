cd536a33cd96760cd97962e14c33451dfba1bd42
이용준 <dldydwns0710@naver.com>
2023-03-21 10:35:24 +0900

    음악추가 페이지 만드는중...

--- src/main/java/com/java/food/dto/GenreDTO.java
+++ src/main/java/com/java/food/dto/GenreDTO.java
@@ -17,6 +17,8 @@ public class GenreDTO {
 	private String imagelink;
 	private String country;
 	private String album_name;
+	private String album_add;
+	private String artist_add;
 	
 	
 	public String getImagelink() {
@@ -25,8 +27,6 @@ public class GenreDTO {
 	public void setImagelink(String imagelink) {
 		this.imagelink = imagelink;
 	}
-	private String album_add;
-	private String artist_add;
 	
 	
 	public String getSongnumber() {
